72d66913c90abe61e1b51e0a03b69a810d0e5b85
meed astitou <astitoumd@gmail.com>
2023-06-20 16:30:58 -0700

    2 pritn alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,24 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
-
-
 void print_alphabet_x10(void)
 {
-	char letter;
+	char ch;
 	int i;
-	for( i = 0; i < 10; i++ )
-	{
 
-		for(letter = 'a' ; letter <= 'z'; letter++)
+
+	i = 0;
+
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(letter);
+			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
