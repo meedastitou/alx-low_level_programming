485a80a63606d16df6f9d5942291c7b9b623e9d9
meed astitou <astitoumd@gmail.com>
2023-06-22 16:54:34 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - that multiplies two integers
  * @a: first input
  * @b: second input
- * Return: multiplication tow input
+ * Return: multiplication result of the 2 inputs
  */
 
 int mul(int a, int b)
