aab3ca61fa5a090bd13e7f844bdf7a082540b954
meed astitou <astitoumd@gmail.com>
2023-06-27 15:02:07 -0700

    101 commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,27 +3,28 @@
 /**
  * _atoi - ascii to integer
  * @s: the specified string
+ * Return: an integer
  */
 
 int _atoi(char *s)
 {
 	char *c;
-	int neg,first_time, nb, curr;
+	int neg, first_time, nb, curr;
 
 	nb = curr = first_time = 0;
 	neg = 1;
-	for (c = s; *c != '\0' ; c++,curr++)
+	for (c = s ; *c != '\0' ; c++, curr++)
 	{
-		if ( *c == '-' && first_time == 0)
+		if (*c == '-' && first_time == 0)
 			neg = neg * -1;
 
-		if ( *c >= '0' && *c <= '9')
+		if (*c >= '0' && *c <= '9')
 		{
 			first_time = 1;
-			nb = (nb * 10 ) + (*c - '0');
+			nb =-1*((nb * 10 ) + (*c - '0'));
 
 		}
-		else if( first_time)
+		else if (first_time)
 			break;
 	}
 	return (neg*nb);
