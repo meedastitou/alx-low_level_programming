a6682135bfff5f264df58852e02b5bf3052b871f
meed astitou <astitoumd@gmail.com>
2023-06-27 07:18:45 -0700

    101 commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,22 +11,26 @@ int _atoi(char *s)
 	int first_time, nb, ten, curr;
 
 	nb = curr = 0;
-	first_time = 1;
+	first_time = 0;
 	ten = 10;
 	for (c = s; *c != '\0' ; c++,curr++)
 	{
 		if ( *c >= '0' && *c <= '9')
 		{
-			if (curr != 0 && first_time == 1 ){
-				if( *(c + curr -1 )== '-')
+			first_time = 1;
+			if (curr != 0 ){
+				if( *(c + curr -1)== '-'){
 					nb = (nb * ten) - (*c - '0');
-				
-				first_time ++;
+					continue;
+				}
+					
+				nb = (nb * ten) + (*c + '0');
 				continue;
+					
 			}
 			nb = (nb * ten) + (*c - '0');
-			first_time++;
-		}
+		}else if(firt_time == 1) 
+			break;
 		
 	}
 	return (nb);
