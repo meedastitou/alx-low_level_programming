9c6b8862eb184961967e2d5233dcf6127f74fcdc
meed astitou <astitoumd@gmail.com>
2023-06-27 04:29:45 -0700

    7 commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 	char *c;
 	int ok = 1;
 
-	for (c = str; *c != '\0' ; c ++, ok++)
+	for (c = str; *c != '\0' ;c ++, ok++)
 	{
-	 	if( *c == '\0') 
+		if ( *c == '\0')
 			break;
-		if(ok % 2 != 0)
+		if (ok % 2 != 0)
 			_putchar(*c);
 	}
 	_putchar('\n');
