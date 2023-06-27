c283681a8afed54db61a4ca9fa602a30b1b994db
meed astitou <astitoumd@gmail.com>
2023-06-26 19:04:40 -0700

    7 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - string other character
+ * @str: secified string
+ */
+
+void puts2(char *str)
+{
+	char *c;
+
+	for (c = str; c != '\0' ; c+=2)
+		_putchar(*c);
+
+}
