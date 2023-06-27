ecd8a63cd8f5ae73e200e49494461386a155c77a
meed astitou <astitoumd@gmail.com>
2023-06-27 15:59:12 -0700

    102 commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,14 +4,14 @@
 
 int main(void)
 {
-	char s[20];
+	char s[15];
 	int i, r;
 
 	srand(time(NULL));
 
 	for (i = 0 ; i < 20 ; i++)
 	{
-		r = rand() % 70;
+		r = rand() % 126;
 		s[i] = (char)r;	
 	}
 	s[i] = '\0';
