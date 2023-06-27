cf5be821c974d490b03753433d1c995e21fc5602
meed astitou <astitoumd@gmail.com>
2023-06-27 15:56:56 -0700

    102 commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
-char* main(void)
+int main(void)
 {
 	char s[20];
 	int i, r;
+
+	srand(time(NULL));
 
 	for (i = 0 ; i < 20 ; i++)
 	{
 		r = rand() % 70;
 		s[i] = (char)r;	
 	}
-	return s;
+	s[i] = '\0';
+	printf("%s", s);
+	return (0);
 }
