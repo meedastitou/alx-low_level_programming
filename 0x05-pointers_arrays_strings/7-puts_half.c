02a490309023c7ecedd8375fbe04fddb975b522a
meed astitou <astitoumd@gmail.com>
2023-06-26 19:49:30 -0700

    8 commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,11 @@ void puts_half(char *str)
 {
 	char *c;
 	int len, i;
-	
+
 	for (c = str; *c != '\0' ; c++)
 		;
 	len = c - str;
-	if (len % 2 != 0 ) _putchar(str[len-1]);
+	if (len % 2 != 0 ) _putchar(str[len - 1]);
 	else{
 		for (i = len / 2 ; i < len ; i++)
 			_putchar(str[i]);
