341b31b41d95caec4ca773772639c936ce6b8b6b
meed astitou <astitoumd@gmail.com>
2023-06-26 18:02:23 -0700

    1 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update value of pointer 
- * @n: a pointer to int
+ *reset_to_98 - update value of pointer 
+ *@n: a pointer to int
  */
 
 void reset_to_98(int *n)
