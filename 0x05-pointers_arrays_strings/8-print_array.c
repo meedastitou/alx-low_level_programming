34d9a7421012a3d9ea8494ad78b4bd56cca2c47c
meed astitou <astitoumd@gmail.com>
2023-06-26 20:02:04 -0700

    9 commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_array - print array
+ * @a: array
+ * @n: element of array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n - 1 ; i++ )
+		printf("%d,",a[i]);
+	printf("%d",a[i])
+
+}
