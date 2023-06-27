784404c5637e144a6c3af7f78260ff00a2c7aa91
meed astitou <astitoumd@gmail.com>
2023-06-26 19:22:20 -0700

    5 commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@ void print_rev(char *s)
 	char *c;
 	int len, i;
 
-        for (c = s ; *c != '\0' ; c++)
+	for (c = s ; *c != '\0' ; c++)
 		;
        
-       	len = c - s;
-
+	len = c - s;
+	
 	for (i = len - 1 ; i >= 0 ; i--)
 		_putchar(s[i]);
 
