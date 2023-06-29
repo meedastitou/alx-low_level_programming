21664e914a60e261471137b031e73c1cd3bf8dbd
meed astitou <astitoumd@gmail.com>
2023-06-29 16:20:14 -0700

    1.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	char *d;
-	int i = 0;
+	int i;
 
 	for (d = dest; *d != '\0' ; d++)
 		;
-	for (;i < n ; i++, d++)
-		*d = s[i];
+	for (i = 0;i < n ; i++, d++)
+		*d = src[i];
 
 	*d = '\0';
 	return (dest);
