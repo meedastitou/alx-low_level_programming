336e93b51676f18021c0644a93dc832299e6b825
meed astitou <astitoumd@gmail.com>
2023-06-29 16:16:12 -0700

    1.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenation function
+ * @dest: the destination string
+ * @src: the soutece string
+ * @n: the size
+ * Return: return concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *d;
+	int i = 0;
+
+	for (d = dest; d != '\0' ; d++)
+		;
+	for (;i < n ; i++, d++)
+		*d = s[i];
+
+	*d = '\0';
+	return (dest);
+
+	
+}
