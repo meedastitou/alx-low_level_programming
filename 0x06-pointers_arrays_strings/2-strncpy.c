3489884b21dc914ef0f8e7848103a35a166797bc
meed astitou <astitoumd@gmail.com>
2023-06-29 16:41:53 -0700

    2.strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy src in dest
+ * @dest: destination string
+ * @src: source string
+ * @n: size
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *s;
+	int i;
+	
+	for ( i = 0 ; src[i] != '\0' && i < n ; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
