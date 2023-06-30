6463180c7ed069fafc603828f8f8c2025f50b95f
meed astitou <astitoumd@gmail.com>
2023-06-29 19:13:17 -0700

    7.Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,7 +8,7 @@
 
 char *leet(char *s)
 {
-	char tab[] = {'o', 'l', ' ', 'e', 'a', ' ', ' ', 't'};
+	char tab[8] = {'o', 'l', ' ', 'e', 'a', ' ', ' ', 't'};
 	int i, j;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
