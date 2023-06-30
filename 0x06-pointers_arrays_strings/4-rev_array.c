3d90833cdb6f02714384dedb8eee80be7fda5c3e
meed astitou <astitoumd@gmail.com>
2023-06-29 17:49:29 -0700

    4.I

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reserver a array
+ * @a: pointer to an array
+ * @n: number of items
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	for ( i = 0, j = n - 1 ; i < j ; i++, j--)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+	return (a);
+
+}
