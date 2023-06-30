f5176523119e74461efd19e38764a99ed703e4e6
meed astitou <astitoumd@gmail.com>
2023-06-29 18:12:32 -0700

    6.Expect

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - captelize all string
+ * @s: specified string
+ * Return: pointer to the captelize string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	s[i] -= 32;
+	i++;
+	for (; s[i] != '\0' ; i++)
+	{
+		if (s[i] != ' ' && s[i - 1] == ' ')
+			s[i] -= 32;
+	}
+	return (s);
+
+}
