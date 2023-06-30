e2c0a5ff507102128944c65af05f614b567e8a90
meed astitou <astitoumd@gmail.com>
2023-06-29 17:37:55 -0700

    3.strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,16 @@ int _strcmp(char *s1, char *s2)
 {
 	char *c1, *c2;
 
-	for (c1 = s1 , c2 = s2; *c1 != '\0'; c1++, c2++)
+	for (c1 = s1, c2 = s2; *c1 != '\0'; c1++, c2++)
 	{
 		if (*c1 < *c2)
 			return (-1);
 		if (*c1 > *c2)
 			return (1);
 	}
-	if(*c2 == '\0')
-	       	return 0;
-	
+
+	if (*c2 == '\0')
+		return (0);
 	return (-1);
 
 }
