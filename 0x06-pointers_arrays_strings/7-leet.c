150f3b9f4f8cb600e5069d50dc485a508fa2f853
meed astitou <astitoumd@gmail.com>
2023-06-29 19:11:44 -0700

    7.Mozart

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodage
+ * @s: sepcified string
+ * Return: pointer to string
+ */
+
+char *leet(char *s)
+{
+	char tab[] = {'o', 'l', ' ', 'e', 'a', ' ', ' ', 't'};
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+			if (s[i] == s[j] && s[j] != ' ')
+				s[i] = j;
+		}
+
+	}
+
+	return (s);
+}
