9db9a46cebcfead00f8dee5b34432b9db5bb0ac5
meed astitou <astitoumd@gmail.com>
2023-07-03 18:48:35 -0700

    5.strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - string string
+ * @haystack: input
+ * @needle: input
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0' ; haystack++)
+	{
+		char *f = haystack;
+		char *s = needle;
+
+		while ( *f == *s && *s != '\0')
+		{
+			f++;
+			s++;
+		}
+		if ( *s == '\0')
+			return (haystack);
+	}
+
+	return ('\0');
+}
