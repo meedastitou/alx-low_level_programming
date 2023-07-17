72e2a7e1f1ee0b813f27de228bf0975aff6c6b81
meed astitou <astitoumd@gmail.com>
2023-07-17 16:51:00 -0700

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef "0-object_like_macro.h"
-#define "0-object_like_macro.h"
+#ifndef _object_like_macro_h_
+#define _object_like_macro_h_
 
 #define SIZE 1024
-#endif
+
+#endif /* _object_like_macro_h_ */
